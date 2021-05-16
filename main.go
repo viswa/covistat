739bbf64bcc3e6365f4d6f14a70fa347e776a115
Viswajith S Valsan <65163117+viswa@users.noreply.github.com>
2021-05-16 20:39:52 +0530

    Add data structures for holding summary

--- main.go
+++ main.go
@@ -14,6 +14,23 @@ const TITLE = `
  ╚═════╝ ╚═════╝   ╚═══╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝   ╚═╝
 `
 
+type Stats struct {
+	Confirmed     int `json:"confirmed"`
+	Recovered     int `json:"recovered"`
+	Active        int `json:"active"`
+	Deceased      int `json:"deceased"`
+	TotalObs      int `json:"total_obs"`
+	HospitalObs   int `json:"hospital_obs"`
+	HomeObs       int `json:"home_obs"`
+	HospitalToday int `json:"hospital_today"`
+}
+
+type Summary struct {
+	Summary     Stats
+	Delta       Stats
+	LastUpdated string `json:"last_updated"`
+}
+
 func main() {
 	fmt.Print(TITLE)
 }
